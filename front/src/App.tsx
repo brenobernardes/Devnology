ea64e45a44eb6031bb9b601a00d7f9eb575d9afd
brenobernardes <breno.pbernardes@gmail.com>
2023-02-10 16:32:34 -0300

    added dashboard component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { GlobalStyled } from './styles/global';
 import { Header } from './Components/Header/index';
+import { Dashboard } from './Components/Dashboard';
 
 function App() {
   return (
     <>
       <GlobalStyled />
       <Header />
+      <Dashboard />
     </>
   );
 }
