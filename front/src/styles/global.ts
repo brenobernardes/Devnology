21fb74cd1549f7f7f5289538b5987e80dd557a36
brenobernardes <breno.pbernardes@gmail.com>
2023-02-10 14:30:48 -0300

    global styles added

---
+++ front/src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+    :root {
+        --background: #063;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box
+    }
+`
