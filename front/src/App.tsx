32ba892feaa0176e817c8d014fbd9922576fa74e
Breno Bernardes <breno.pbernardes@gmail.com>
2023-02-11 17:10:46 -0300

    modal component import to app.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalStyled } from './styles/global';
 import { Header } from './Components/Header/index';
+import { NewRegisterModal } from './Components/Modal';
 import { TableList } from './Components/TableList';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  };
+
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <GlobalStyled />
-      <Header />
+      <Header onOpenModal={handleOpenModal} />
       <TableList />
+      <NewRegisterModal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
+      />
     </>
   );
 }
