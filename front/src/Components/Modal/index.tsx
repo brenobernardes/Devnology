ca636c7fabe26c6f5599b0252a5b4a0078812fb0
brenobernardes <breno.pbernardes@gmail.com>
2023-02-15 15:47:11 -0300

    style adjustments

--- front/src/Components/Modal/index.tsx
+++ front/src/Components/Modal/index.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Modal from "react-modal";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import axios from "axios";
-import { TableListProps } from "../../Interface/TableListProps";
+import { Container } from "./styles";
+// import { TableListProps } from "../../Interface/TableListProps";
 
 interface newRegisterModalProps {
     isOpen: boolean;
@@ -51,38 +52,42 @@ export function NewRegisterModal({ isOpen, onRequestClose }: newRegisterModalPro
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <Form>
-                <h2>Salvar Link</h2>
+            <Container>
+                <Form>
+                    <h2>Salvar Link</h2>
 
-                <Form.Group>
-                    <Form.Label>Título</Form.Label>
-                        <Form.Control
-                            name="title"
-                            type="text"
-                            value={data.title}
-                            onChange={onChangeInput} 
-                        />
+                    <Form.Group>
+                        <Form.Label>Título</Form.Label>
+                            <Form.Control
+                                name="title"
+                                type="text"
+                                value={data.title}
+                                onChange={onChangeInput} 
+                            />
 
-                    <Form.Label>URL</Form.Label>
-                        <Form.Control
-                            name="url"
-                            type="text"
-                            value={data.url}
-                            onChange={onChangeInput}
-                        />
+                        <Form.Label>URL</Form.Label>
+                            <Form.Control
+                                name="url"
+                                type="text"
+                                value={data.url}
+                                onChange={onChangeInput}
+                            />
 
-                    <Form.Label>Descrição</Form.Label>
-                        <Form.Control
-                            name="description"
-                            type="text"
-                            value={data.description}
-                            onChange={onChangeInput}
-                        />
-                </Form.Group>
-            </Form>
+                        <Form.Label>Descrição</Form.Label>
+                            <Form.Control
+                                name="description"
+                                type="text"
+                                value={data.description}
+                                onChange={onChangeInput}
+                            />
+                    </Form.Group>
+                </Form>
 
-            <Button onClick={sendData}>Salvar</Button>
+                <Button onClick={sendData}>Salvar</Button>
+            </Container>
         </Modal>
     );
 }
---
+++ front/src/Components/Modal/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    margin: 0;
+    padding: 0;
+
+    h2 {
+        text-align: center;
+    }
+
+    Button {
+        margin-top: 1rem;
+    }
+`
