eedde71e4db55020ea59efeff775879b6167fa8f
brenobernardes <breno.pbernardes@gmail.com>
2023-02-15 15:48:11 -0300

    styled adjustments

--- front/src/styles/global.ts
+++ front/src/styles/global.ts
@@ -7,6 +7,7 @@ export const GlobalStyled = createGlobalStyle`
         --black: #000;
         --grey: #1C1C1C;
         --white: #FFFF;
+        --red: #FF0000;
     }
 
     * {
@@ -14,5 +15,27 @@ export const GlobalStyled = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
         background: var(--table-background);
+    }
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.24rem;
     }
 `
